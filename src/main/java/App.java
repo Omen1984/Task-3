4d164987fc95f46d307a104203f42c6fc1578266
Omen1984 <ivan.plehanov42@yandex.ru>
2023-02-26 16:15:38 +0500

    Task complited

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanOne =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catOne =
+                (Cat) applicationContext.getBean("cat");
+        Cat catTwo =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean isOneHello = beanOne == beanTwo;
+        boolean isOneCat = catOne == catTwo;
+
+        System.out.println("Тест BeanScope singleton = " + isOneHello);
+        System.out.println("Тест BeanScope prototype = " + isOneCat);
+
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+
+    private String voice;
+
+    public Cat() {
+    }
+
+    public Cat(String voice) {
+        this.voice = voice;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String message) {
+        this.voice = message;
+    }
+
+}
